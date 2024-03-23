bbf2d76e4532acf1e38c9e914ee35efa0cb644a7
EemanNaseer1 <eemannaseer1@gmail.com>
2024-03-23 07:06:22 +0500

    assignment 5

---
+++ work3.dart
@@ -0,0 +1,42 @@
+void main() {
+  List<String> favoriteFruit = [
+    "orange",
+    "banana",
+    "apple",
+    "strawberry",
+    "watermelon",
+    "kiwi",
+    "mango"
+  ];
+
+  favoriteFruit.sort();
+  print(favoriteFruit);
+
+  List num = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20
+  ];
+  print(num);
+
+  List numbers = [2, 5, 6, 3, 1, 3, 8, 2];
+  numbers.sort();
+  print(numbers);
+}
