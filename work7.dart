530570c3f535d3d2019935c69fb51d9df96cd76a
EemanNaseer1 <eemannaseer1@gmail.com>
2024-05-04 15:29:17 +0500

    assignment

--- work7.dart
+++ work7.dart
@@ -1,12 +1,23 @@
 void main() {
   //Q.1
-  for (int i = 0; i < 10; i++) {
-    print("Fibonacci sequence upto 10: ${i - 1}");
+  for (int i = 0; i < 10; -i) {
+    print("Fibonacci sequence upto 10: ${i}");
   }
 
   //Q.2
   List number = [3, 9, 1, 6, 4, 2, 8, 5, 7];
-  for (int i = 1; i >= 9; ++i) {
-    print(number[i]);
+  var largestValue = number[0];
+  for (int i = 0; i < number.length; i++) {
+    if (number[i] > largestValue) {
+      largestValue = number[i];
+      print(largestValue);
+    }
   }
+
+  //Q.3
+  for (int i = 1; i <= 10; i++) {
+    print("5*$i = ${5 * i}");
+  }
+
+  //Q.4
 }
